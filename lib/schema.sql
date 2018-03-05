b4f3cb530a078168ca2223d83978c4427aa033da
Juan Rivera <juanriv45@gmail.com>
2018-03-05 05:49:04 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -7,21 +7,26 @@ CREATE TABLE series(
 
 CREATE TABLE sub_genres(
   id INTEGER PRIMARY KEY,
-  
+  name TEXT
 );
 
 CREATE TABLE authors(
   id INTEGER PRIMARY KEY,
-  
+  name TEXT
 );
 
 CREATE TABLE books(
   id INTEGER PRIMARY KEY,
-  
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
 );
 
 CREATE TABLE characters(
   id INTEGER PRIMARY KEY,
-  
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER  
 );
-
