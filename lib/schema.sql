e834d6eb556168f4c0c6e0f020dfd695b494da58
Juan Rivera <juanriv45@gmail.com>
2018-03-05 05:45:20 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE series(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  sub_genre_id INTEGER
+);
+
+CREATE TABLE sub_genres(
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE authors(
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE books(
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE characters(
+  id INTEGER PRIMARY KEY,
+  
+);
+
